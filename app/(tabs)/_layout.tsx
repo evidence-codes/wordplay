9cc0eb1fb385368800d9748be9d1648501af6338
Evidence Adejuwon <adejuwonevidence181@gmail.com>
2025-06-09 18:44:28 +0100

    feat: create home screen

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Ionicons from "@expo/vector-icons/Ionicons";
 import { Link, Tabs } from "expo-router";
-import { Pressable } from "react-native";
+import { Pressable, StatusBar } from "react-native";
 
 import Colors from "@/constants/Colors";
 import { useColorScheme } from "@/components/useColorScheme";
@@ -9,56 +10,63 @@ import { useClientOnlyValue } from "@/components/useClientOnlyValue";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>["name"];
+  name: React.ComponentProps<typeof Ionicons>["name"];
   color: string;
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  return <Ionicons size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs
-    // screenOptions={{
-    //   tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
-    //   // Disable the static render of the header on web
-    //   // to prevent a hydration error in React Navigation v6.
-    //   headerShown: useClientOnlyValue(false, true),
-    // }}
-    >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
-          headerShown: false,
+    <>
+      <StatusBar backgroundColor="#3E3BEE" barStyle="dark-content" />
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "#3E3BEE",
+          // Disable the static render of the header on web
+          // to prevent a hydration error in React Navigation v6.
+          headerShown: useClientOnlyValue(false, true),
         }}
-      />
-      <Tabs.Screen
-        name="puzzle"
-        options={{
-          title: "Puzzle",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerShown: false,
-        }}
-      />
-      <Tabs.Screen
-        name="vocabulary"
-        options={{
-          title: "Vocabulary",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerShown: false,
-        }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerShown: false,
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+            headerShown: false,
+          }}
+        />
+        <Tabs.Screen
+          name="puzzle"
+          options={{
+            title: "Puzzle",
+            tabBarIcon: ({ color }) => (
+              <TabBarIcon name="extension-puzzle" color={color} />
+            ),
+            headerShown: false,
+          }}
+        />
+        <Tabs.Screen
+          name="vocabulary"
+          options={{
+            title: "Vocabulary",
+            tabBarIcon: ({ color }) => <TabBarIcon name="book" color={color} />,
+            headerShown: false,
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            tabBarIcon: ({ color }) => (
+              <TabBarIcon name="person-circle" color={color} />
+            ),
+            headerShown: false,
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,51 @@
-import { SafeAreaView, View } from "react-native";
+import { SafeAreaView, View, Text, Image } from "react-native";
 
 export default function Home() {
   return (
-    <SafeAreaView>
-      <View></View>
+    <SafeAreaView className="bg-white flex-1">
+      <View className="bg-[#3E3BEE] px-10">
+        <View className="pt-16 flex-row items-center justify-between">
+          <View>
+            <Text className="text-[36px] font-instrument_bold text-white">
+              Hi, James
+            </Text>
+          </View>
+          <View>
+            <Image
+              source={require("../../assets/images/default-image.png")}
+              className="w-[50px] h-[50px] rounded-full border-2 border-white self-center"
+            />
+          </View>
+        </View>
+        <View>
+          <Text className="text-[16px] font-instrument_regular text-white">
+            Good Afternoon
+          </Text>
+        </View>
+
+        <View className="flex-1 bg-white border-b-[1px] border-b-white mt-4 mb-4"></View>
+
+        <View className="flex-row justify-around mb-4">
+          <View>
+            <Text className="text-[16px] text-white font-instrument_regular">
+              Score
+            </Text>
+            <Text className="text-[30px] text-white font-instrument_bold">
+              334 XP
+            </Text>
+          </View>
+          <View>
+            <Text className="text-[16px] text-white font-instrument_regular">
+              Streak
+            </Text>
+            <Text className="text-[30px] text-white font-instrument_bold">
+              27 Days
+            </Text>
+          </View>
+        </View>
+
+        <View className="flex-1 bg-white border-b-[1px] border-b-white mt-2 mb-2"></View>
+      </View>
     </SafeAreaView>
   );
 }
