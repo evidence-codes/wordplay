5d429fbbf59573368cb1a0fe2b2a4b16367c229b
Evidence Adejuwon <adejuwonevidence181@gmail.com>
2025-06-17 10:25:55 +0100

    feat: implemented firebase for storage management

---
+++ config/firebase.ts
@@ -0,0 +1,27 @@
+// import { initializeApp, getApps, deleteApp } from "firebase/app";
+// import "firebase/auth";
+// import { initializeAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAwfb6Pp1u4ViVse8Ra1Y2JNP7DdL3R2D0",
+//   authDomain: "wordplay-82803.firebaseapp.com",
+//   projectId: "wordplay-82803",
+//   storageBucket: "wordplay-82803.appspot.com",
+//   messagingSenderId: "702823521994",
+//   appId: "1:702823521994:web:358ec77c5633458ed19e33",
+//   measurementId: "G-SZ3V95XE4M",
+// };
+
+// // Clean slate approach - delete existing apps first
+// const existingApps = getApps();
+// if (existingApps.length > 0) {
+//   existingApps.forEach((app) => deleteApp(app));
+// }
+
+// // Fresh initialization
+// const app = initializeApp(firebaseConfig);
+// const auth = initializeAuth(app);
+// const db = getFirestore(app);
+
+// export { auth, db };
